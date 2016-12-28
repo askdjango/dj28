c63e0a82f4ef2f6bf5e53087f6b70ff41883c7d6
Chinseok Lee <allieuslee@gmail.com>
2016-12-28 14:56:36 +0900

    Admin Custom

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,11 @@ from .models import Post, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'content_size']
+    list_display_links = ['id', 'title']
+
+    def content_size(self, post):
+        return '{}글자'.format(len(post.content))
 
 
 @admin.register(Comment)
