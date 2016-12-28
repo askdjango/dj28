1ccea0b762b05c3caf116330f4e134567a66c601
Chinseok Lee <allieuslee@gmail.com>
2016-12-28 15:22:20 +0900

    PostAdmin의 queryset에 comment_set prefetch_related 적용

--- blog/admin.py
+++ blog/admin.py
@@ -4,11 +4,21 @@ from .models import Post, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'content_size']
+    list_display = ['id', 'title', 'content_size', 'detail']
     list_display_links = ['id', 'title']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.prefetch_related('comment_set')
 
     def content_size(self, post):
         return '{}글자'.format(len(post.content))
+
+    def detail(self, post):
+        id_list = ','.join(
+            str(comment.id)
+            for comment in post.comment_set.all())
+        return 'comment id list : {}'.format(id_list)
 
 
 @admin.register(Comment)
